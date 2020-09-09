eb6cdb1c4a8737400633cc4b13f9b08dec0b663c
Luis Sousa <lv-ss@hotmail.com>
2020-09-09 04:58:03 -0300

    first commit

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,24 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+     Enum.map(dna, 
+      fn x -> 
+        case x do
+          ?A -> ?U
+          ?C -> ?G
+          ?T -> ?A
+          ?G -> ?C
+          _ -> "Error"
+        end
+      end
+       )
+  end
+end
